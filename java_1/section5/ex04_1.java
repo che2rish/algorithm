26fb27c80882e20dc33f9eb565b33edba6289819
che2rish <whdkfka101@naver.com>
2022-04-07 16:23:09 +0900

    04.후위식 연산(postfix)

---
+++ java_1/section5/ex04_1.java
@@ -0,0 +1,33 @@
+package section5;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class ex04_1 {
+    public int solution(String str){
+        int answer = 0;
+        Stack<Integer> stack = new Stack<>();
+
+        for(char x : str.toCharArray()){
+            if(Character.isDigit(x)) stack.push(x-48);
+            else{
+                int rt = stack.pop();
+                int lt = stack.pop();
+                if(x == '+') stack.push(lt + rt);
+                else if (x == '-') stack.push(lt - rt);
+                else if (x == '*') stack.push(lt * rt);
+                else if (x == '/') stack.push(lt / rt);
+            }
+        }
+
+        answer = stack.get(0);
+        return answer;
+    }
+
+    public static void main(String[] args){
+        ex04_1 T = new ex04_1();
+        Scanner kb = new Scanner(System.in);
+        String str = kb.next();
+        System.out.println(T.solution(str));
+    }
+}
