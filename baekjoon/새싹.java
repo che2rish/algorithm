cd87bc99a3e418ea9d4171f8585ddb10825368f3
che2rish <whdkfka101@naver.com>
2022-06-25 19:16:21 +0900

    25083. 새싹

---
+++ baekjoon/새싹.java
@@ -0,0 +1,13 @@
+package baekjoon;
+
+public class 새싹 {
+    // ",\ => 출력 하려면 \ 추가
+    public static void main(String[] args) {
+        System.out.println("         ,r'\"7");
+        System.out.println("r`-_   ,'  ,/");
+        System.out.println(" \\. \". L_r'");
+        System.out.println("   `~\\/");
+        System.out.println("      |");
+        System.out.println("      |");
+    }
+}
